1a2f4e2f014f3dfbeb8091c48aca55a20f8b93a5
myles.novick@gmail.com <myles.novick@gmail.com>
2021-04-21 18:39:54 -0400

    for the build machine

--- airflow/scripts/webserver-entrypoint.sh
+++ airflow/scripts/webserver-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 cp /mnt/airflow-conf/airflow.cfg /opt/airflow/airflow.cfg
+until airflow db check-migrations; do sleep 5; done
 airflow users create --username splice --lastname machine --firstname splice --email cloud@splicemachine.com --role Admin --password $SPLICE_ADMIN_PWD || true
 airflow webserver
