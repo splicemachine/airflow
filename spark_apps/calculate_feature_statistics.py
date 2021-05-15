6e05b737f34984f9a5ad6eb33a85600f18b8d4b7
myles.novick@gmail.com <myles.novick@gmail.com>
2021-05-15 18:04:36 -0400

    way better

--- spark_apps/calculate_feature_statistics.py
+++ spark_apps/calculate_feature_statistics.py
@@ -2,12 +2,9 @@ from os import environ as env_vars, popen
 from pyspark.sql import SparkSession
 from splicemachine.spark import ExtPySpliceContext
 from splicemachine.features import FeatureStore
-from splicemachine.features.constants import FeatureType
 from pyspark_dist_explore import pandas_histogram
-import pyspark.sql.functions as F
 from datetime import datetime
 import sys
-from functools import reduce
 import numpy as np
 import pandas as pd
 import json
@@ -22,14 +19,6 @@ sc = spark.sparkContext
 log4jLogger = sc._jvm.org.apache.log4j
 LOGGER = log4jLogger.LogManager.getLogger(__name__)
 LOGGER.warn("pyspark script logger initialized")
-
-def median(row, df):
-    name = row['index']
-    try:
-        return df.approxQuantile(name, [0.5], 0)[0]
-    except Exception as e:
-        LOGGER.warn(f'Cannot calculate median for column {name} - {e}')
-        return None
 
 def cardinality(row, df):
     name = row['index']
@@ -48,7 +37,7 @@ def histogram(row, df, splice):
         sql = f'select feature_histogram from featurestore.feature_stats where feature_id = {f_id} order by last_update_ts desc {{limit 1}}'
         previous = splice.df(sql)
         if previous.count() == 0:
-            iqr = reduce(lambda x,y: x-y, df.approxQuantile(name, [0.75, 0.25], 0))
+            iqr = row['75%'] - row['25%']
             bin_width = int(2 * (iqr / (row['count'] ** (1. / 3))))
             f_min = row['min']
             f_max = row['max']
@@ -96,92 +85,42 @@ def calculate_statistics(fset):
     LOGGER.warn(f"Getting features in feature set '{fset}'")
     features = fs.get_features_from_feature_set(schema, table)
 
-    # stats = pd.DataFrame(columns=['feature_id', 'feature_cardinality', 'feature_histogram', 'feature_mean', 
-    #             'feature_median', 'feature_count', 'feature_stddev', 'last_update_ts', 'last_update_username'])
     update_time = datetime.now()
     
-    stats = df.select([f.name for f in features]).summary().toPandas().set_index('summary').T.reset_index()
+    stats = df.select([f.name for f in features]).summary().toPandas().\
+        set_index('summary').T.rename_axis('name').rename_axis(None, axis=1).reset_index()
+    cols = list(stats.columns)[1:]
+    stats[cols] = stats[cols].apply(pd.to_numeric, errors='coerce')
 
     stats['feature'] = stats.apply(lambda row: next((f for f in features if f.name.upper() == row['index'].upper()), None), axis=1)
     stats['feature_id'] = stats.apply(lambda row: row['feature'].feature_id, axis=1)
-    stats['feature_median'] = stats.apply(lambda row: median(row, df), axis=1)
     stats['feature_cardinality'] = stats.apply(lambda row: cardinality(row, df), axis=1)
     stats['feature_histogram'] = stats.apply(lambda row: histogram(row, df, splice), axis=1)
     
-    stats.rename(columns={'count': 'feature_count', 'mean': 'feature_mean', 'stddev': 'feature_stddev'}, inplace=True)
+    rename = {
+        'count': 'feature_count',
+        'mean': 'feature_mean',
+        'stddev': 'feature_stddev',
+        '50%': 'feature_median',
+        '25%': 'feature_q1',
+        '75%': 'feature_q3',
+        'min': 'feature_min',
+        'max': 'feature_max'
+    }
+    stats.rename(columns=rename, inplace=True)
     stats['last_update_ts'] = update_time
     stats['last_update_username'] = 'airflow'
     stats.rename(columns=str.upper, inplace=True)
     
     fs_df = splice.df('select * from featurestore.feature_stats')
     stats = stats[fs_df.columns]
-    # for feature in features:
-    #     name = feature.name
-    #     f_id = feature.feature_id
 
-    #     LOGGER.warn(f"Calculating statistics for feature '{name}'")
-    #     count = df.select(name).count()
-
-    #     if feature.is_continuous():
-    #         sql = f'select feature_histogram from featurestore.feature_stats where feature_id = {f_id} order by last_update_ts desc {{limit 1}}'
-    #         previous = splice.df(sql)
-    #         if previous.count() == 0:
-    #             iqr = reduce(lambda x,y: x-y, df.approxQuantile(name, [0.75, 0.25], 0))
-    #             bin_width = int(2 * (iqr / (count ** (1. / 3))))
-    #             f_min = df.select(F.min(df[name])).first()[0]
-    #             f_max = df.select(F.max(df[name])).first()[0]
-    #             if f_min == f_max:
-    #                 bins = []
-    #             elif bin_width > 0:
-    #                 bins = list(np.arange(f_min, f_max, bin_width))
-    #             else:
-    #                 bins = list(np.linspace(f_min, f_max, 10, False))
-    #             bins = [float('-inf')] + bins + [f_max, float('inf')]
-    #             histogram = pandas_histogram(df.select(name), bins=bins)
-    #             histogram = histogram / np.sum(histogram)
-    #             hist_json = histogram[name].to_json()
-    #         else:
-    #             old = json.loads(previous.first()[0])
-    #             intervals = set()
-    #             [intervals.update(key.split(' - ')) for key in old.keys()]
-    #             bins = [float(interval) for interval in list(intervals)]
-    #             bins.sort()
-    #             histogram = pandas_histogram(df.select(name), bins=bins)
-    #             histogram = histogram / np.sum(histogram)
-    #             hist_json = histogram[name].to_json()
-    #     else:
-    #         data_type = feature.feature_data_type['data_type'].upper()
-    #         if data_type in ['DATE', 'TIME', 'TIMESTAMP']:
-    #             LOGGER.warn(f"Cannot create histogram for data type {data_type}'")
-    #             hist_json = None
-    #         else:
-    #             histogram = df.groupBy(name).count().toPandas().set_index(name)
-    #             histogram = histogram / np.sum(histogram)
-    #             hist_json = json.dumps(histogram.to_dict()['count'])
-
-    #     cardinality = df.select(name).distinct().count()
-    #     mean = df.select(F.avg(df[name])).first()[0]
-    #     median = df.approxQuantile(name, [0.5], 0)[0]
-    #     stddev = df.select(F.stddev(df[name])).first()[0]
-
-    #     row = {
-    #         'feature_id': f_id,
-    #         'feature_cardinality': cardinality,
-    #         'feature_histogram': hist_json,
-    #         'feature_mean': mean,
-    #         'feature_median': median,
-    #         'feature_count': count,
-    #         'feature_stddev': stddev,
-    #         'last_update_ts': update_time,
-    #         'last_update_username': 'airflow'
-    #     }
-    #     LOGGER.warn(f"Statistics calculated for feature '{name}' - adding to DataFrame")
-    #     stats = stats.append(row, ignore_index=True)
     LOGGER.warn(f"Statistics calculated for all features")
-    stats_df = splice.createDataFrame(stats, None)
+    stats_df = splice.createDataFrame(stats, fs_df.schema)
     LOGGER.warn(f"Pushing statistics to FeatureStore.Feature_Stats")
-    splice.mergeInto(stats_df, 'FEATURESTORE.FEATURE_STATS')
+    splice.insert(stats_df, 'FEATURESTORE.FEATURE_STATS')
     LOGGER.warn(f"Statistics pushed to database")
+    # This log is necessary for airflow to recognize successful executions
     LOGGER.warn('Exit code: 0')
     spark.stop()
 
